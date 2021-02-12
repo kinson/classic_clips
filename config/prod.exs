1ae930254c9caf55e8c358e78013d268214cb68f
kinson <swilkinsonhunter@gmail.com>
2021-02-11 18:22:47 -0700

    update endpoint url with new domain

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,7 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :classic_clips, ClassicClipsWeb.Endpoint,
-  url: [host: "classicfactory.art", port: 80],
+  url: [host: "classicclipsinc.com", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
