b41d645316ea159b6f29b3e81758ce9e15499ae0
kinson <swilkinsonhunter@gmail.com>
2021-02-21 15:39:03 -0700

    add a few more tag seeds

--- priv/repo/tags.exs
+++ priv/repo/tags.exs
@@ -7,9 +7,14 @@ tags = [
   %{name: "Trey", code: "jd", type: "crew"},
   %{name: "Tas", code: "jd", type: "crew"},
   %{name: "Leigh", code: "jd", type: "crew"},
-  %{name: "Out of Context", code: "out-of-context", type: "topics"},
   %{name: "Leigh Story", code: "leigh-story", type: "topics"},
   %{name: "Hot Take", code: "hot-take", type: "topics"},
+  %{name: "Out of Context", code: "out-of-context", type: "topics"},
+  %{name: "Wedgie", code: "wedgie", type: "topics"},
+  %{name: "Wedgie", code: "wedgie", type: "topics"},
+  %{name: "Big Beef", code: "big-beef", type: "topics"},
+  %{name: "Prediction", code: "prediction", type: "topics"},
+  %{name: "Classic Drop", code: "classic-drop", type: "topics"}
 ]
 
 taggers = Enum.map(tags, &(Tag.changeset(%Tag{}, &1)))
