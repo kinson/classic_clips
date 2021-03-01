6aa66b5e51727415777c87a85eb67503fb9a9651
kinson <swilkinsonhunter@gmail.com>
2021-02-28 19:33:15 -0700

    update big beef archive page title

--- lib/classic_clips_web/controllers/big_beef_controller.ex
+++ lib/classic_clips_web/controllers/big_beef_controller.ex
@@ -5,6 +5,7 @@ defmodule ClassicClipsWeb.BigBeefController do
     big_beefs = ClassicClips.BigBeef.list_big_beef_events()
 
     assign(conn, :big_beefs, big_beefs)
+    |> assign(:page_title, "BIG BEEF ARCHIVE")
     |> render(:index)
   end
 end
