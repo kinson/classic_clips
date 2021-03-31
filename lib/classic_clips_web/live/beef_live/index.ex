9a746073048e67ebdb633734568a6a49de154210
kinson <swilkinsonhunter@gmail.com>
2021-03-30 23:49:01 -0600

    initial commit for bbt-v2

--- lib/classic_clips_web/live/beef_live/index.ex
+++ lib/classic_clips_web/live/beef_live/index.ex
@@ -3,18 +3,22 @@ defmodule ClassicClipsWeb.BeefLive.Index do
 
   alias ClassicClips.BigBeef
   alias ClassicClips.BigBeef.Beef
+  alias ClassicClips.BigBeef.{Player, BigBeefEvent}
 
   @impl true
   def mount(_params, _session, socket) do
     if connected?(socket), do: BigBeef.subscribe_new_beef()
 
     active_game_count = get_active_game_count()
+    big_beefs = ClassicClips.BigBeef.list_big_beef_events()
 
     modified_socket =
       socket
       |> assign(:beefs, BigBeef.get_recent_beefs(active_game_count))
       |> assign(:last_updated, current_datetime())
       |> assign(:active_game_count, active_game_count)
+      |> assign(:big_beefs, big_beefs)
+      |> assign(:page_type, "live")
 
     {:ok, modified_socket}
   end
@@ -61,6 +65,14 @@ defmodule ClassicClipsWeb.BeefLive.Index do
     {:noreply, assign(socket, :beefs, list_beefs())}
   end
 
+  def handle_event("select-archive", _, socket) do
+    {:noreply, assign(socket, :page_type, "archive")}
+  end
+
+  def handle_event("select-live", _, socket) do
+    {:noreply, assign(socket, :page_type, "live")}
+  end
+
   defp list_beefs do
     BigBeef.list_beefs()
   end
@@ -71,5 +83,55 @@ defmodule ClassicClipsWeb.BeefLive.Index do
 
   defp get_active_game_count() do
     ClassicClips.BeefServer.get_active_game_count()
+  end
+
+  def format_time(time) do
+    six_hour_back_offset = -1 * 60 * 60 * 6
+
+    time
+    |> DateTime.add(six_hour_back_offset, :second)
+    |> DateTime.to_date()
+    |> Date.to_string()
+  end
+
+  def name(%Player{first_name: first_name, last_name: last_name}) do
+    "#{last_name}, #{first_name}"
+  end
+
+  def count(%Beef{beef_count: count}), do: count
+
+  def bs_link(%BigBeefEvent{box_score_url: url}) do
+    case String.contains?(url, "https://") do
+      true -> url
+      false -> "#"
+    end
+  end
+
+  def yt_link(%BigBeefEvent{yt_highlight_video_url: url}) do
+    case String.contains?(url, "https://") do
+      true -> url
+      false -> "#"
+    end
+  end
+
+  def yt_text(%BigBeefEvent{yt_highlight_video_url: url}) do
+    case String.contains?(url, "https://") do
+      true -> "Big Beef Highlights"
+      false -> "Coming Soon..."
+    end
+  end
+
+  def bs_text(%BigBeefEvent{box_score_url: url}) do
+    case String.contains?(url, "https://") do
+      true -> "Box Score"
+      false -> "Coming Soon..."
+    end
+  end
+
+  def is_active(page, name) do
+    case page == name do
+      true -> "show"
+      false -> ""
+    end
   end
 end
