7cc9d612135d09a41d9c9be198309b845cefcdfe
kinson <swilkinsonhunter@gmail.com>
2021-04-02 15:40:08 -0600

    load the video

--- lib/classic_clips/timeline.ex
+++ lib/classic_clips/timeline.ex
@@ -369,8 +369,11 @@ defmodule ClassicClips.Timeline do
       from(c in Clip, where: c.id == ^clip_id, select: c)
       |> Repo.update_all(inc: [vote_count: 1])
       |> case do
-        {1, [clip]} -> {:ok, Repo.preload(clip, :user)}
-        error -> error
+        {1, [clip]} ->
+          {:ok, Repo.preload(clip, :user) |> Repo.preload(:tags) |> Repo.preload(:video)}
+
+        error ->
+          error
       end
 
     {:ok, vote} =
