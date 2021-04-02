de171d186c80aa398602933ea14a2bfa87b9fde9
kinson <swilkinsonhunter@gmail.com>
2021-04-02 10:12:39 -0600

    change bounds

--- lib/classic_clips/classics/services/youtube.ex
+++ lib/classic_clips/classics/services/youtube.ex
@@ -59,12 +59,12 @@ defmodule ClassicClips.Classics.Services.Youtube do
 
     published_after_date =
       DateTime.utc_now()
-      |> DateTime.add(-1 * one_day * 60, :second)
+      |> DateTime.add(-1 * one_day * 30, :second)
       |> DateTime.to_iso8601()
 
     published_before_date =
       DateTime.utc_now()
-      |> DateTime.add(-1 * one_day * 29, :second)
+      |> DateTime.add(-1 * one_day * 20, :second)
       |> DateTime.to_iso8601()
 
     published_after =
