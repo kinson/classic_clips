33161eb53b7d64731a4d1e3fa04eb340099a97c9
kinson <swilkinsonhunter@gmail.com>
2021-04-11 22:09:53 -0600

    update pledge amount and remove some extra fonts

--- lib/classic_clips_web/live/beef_live/beef_component.ex
+++ lib/classic_clips_web/live/beef_live/beef_component.ex
@@ -12,9 +12,9 @@ defmodule ClassicClipsWeb.BeefLive.BeefComponent do
      <div class="beef-point" style="<%= "left: #{(@beef.game_time / 3600) * 100}%;top:#{100 - (((@beef.beef_count - 5)/21) * 100)}%;" %>">
       <p class="steak">&#129385;</p>
       <div class="beef-info">
-        <p class="l-name"><%= @beef.player_last_name %>, <%= @beef.player_first_name %></p>
+        <p class="l-name"><%= @beef.player_first_name %> <%= @beef.player_last_name %></p>
         <p class="l-name">Beef Count: <%= @beef.beef_count %></p>
-        <p class="l-name">Game Progress: <%= (@beef.game_time / (60 *48)) * 100 |> trunc() %>%</p>
+        <p class="l-name">Game Progress: <%= (@beef.game_time / (60 * 48)) * 100 |> trunc() %>%</p>
       </div>
      </div>
     """
