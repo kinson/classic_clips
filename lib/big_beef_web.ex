8d20f8affc8e7f0f74e69ca2aa850e46e6b899b0
kinson <swilkinsonhunter@gmail.com>
2021-05-08 21:49:10 -0600

    better latest beef styling for mobile

--- lib/big_beef_web.ex
+++ lib/big_beef_web.ex
@@ -83,7 +83,6 @@ defmodule BigBeefWeb do
 
       # Import LiveView helpers (live_render, live_component, live_patch, etc)
       import Phoenix.LiveView.Helpers
-      import BigBeefWeb.LiveHelpers
 
       # Import basic rendering functionality (render, render_layout, etc)
       import Phoenix.View
