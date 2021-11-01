4e0f33be505a685516599dbe01db5583fcbfe062
kinson <swilkinsonhunter@gmail.com>
2021-10-31 20:17:27 -0600

    Some user feedback

--- lib/pick_em_web/live/pick_em/index.ex
+++ lib/pick_em_web/live/pick_em/index.ex
@@ -20,6 +20,10 @@ defmodule PickEmWeb.PickEmLive.Index do
 
     {selected_team, selection_saved} = get_selected_team(user_pick)
 
+    can_save_pick? = can_save_pick?(matchup)
+
+    total_picks_today = ClassicClips.PickEm.get_pick_count_for_matchup(matchup)
+
     {:ok,
      socket
      |> assign(:matchup, matchup)
@@ -28,10 +32,16 @@ defmodule PickEmWeb.PickEmLive.Index do
      |> assign(:user_pick, user_pick)
      |> assign(:selected_team, selected_team)
      |> assign(:selection_saved, selection_saved)
+     |> assign(:can_save_pick?, can_save_pick?)
+     |> assign(:total_picks_today, total_picks_today)
      |> assign(:google_auth_url, generate_oauth_url())}
   end
 
   @impl true
+  def handle_event(_, %{"enabled" => "false"}, socket) do
+    {:noreply, socket}
+  end
+
   def handle_event("away-click", _, socket) do
     {:noreply, assign(socket, :selected_team, socket.assigns.matchup.away_team)}
   end
@@ -44,7 +54,19 @@ defmodule PickEmWeb.PickEmLive.Index do
     %{user_pick: user_pick, selected_team: selected_team, user: user, matchup: matchup} =
       socket.assigns
 
-    ClassicClips.PickEm.save_user_pick(user_pick, selected_team, user, matchup)
+    socket =
+      if can_save_pick?(socket.assigns.matchup) do
+        case ClassicClips.PickEm.save_user_pick(user_pick, selected_team, user, matchup) do
+          {:ok, user_pick} ->
+            socket
+            |> assign(:user_pick, user_pick)
+            |> assign(:success_message, "Saved your pick")
+
+          {:error, _} ->
+            assign(socket, :error_message, "Could not save your pick, please try again")
+        end
+      end
+
     {:noreply, socket}
   end
 
@@ -80,13 +102,24 @@ defmodule PickEmWeb.PickEmLive.Index do
   def get_ndc_pick("leigh", %NdcPick{leigh_pick_team: team}), do: team.abbreviation
   def get_ndc_pick("trey", %NdcPick{trey_pick_team: team}), do: team.abbreviation
 
-  def get_time_left(%MatchUp{tip_datetime: tip_datetime}) do
-    if DateTime.compare(DateTime.utc_now(), tip_datetime) == :lt do
+  def get_time_left(%MatchUp{tip_datetime: tip_datetime} = matchup) do
+    if can_save_pick?(matchup) do
       "#{div(DateTime.diff(tip_datetime, DateTime.utc_now()), 60)} minutes left to pick"
     else
       "The time to pick has passed"
     end
   end
+
+  def can_save_pick?(%MatchUp{tip_datetime: tip_datetime}) do
+    DateTime.compare(DateTime.utc_now(), tip_datetime) == :lt
+  end
+
+  def get_save_button_text(_, false), do: "Pick No Longer Available"
+  def get_save_button_text(false, _), do: "Lock It In"
+  def get_save_button_text(true, _), do: "Update Your Pick"
+
+  def maybe_disable(class_string, false), do: class_string <> " opacity-60"
+  def maybe_disable(class_string, _), do: class_string <> " shadow-lg"
 
   def get_team_button_style(nil, _) do
     get_team_button_class("not selected")
@@ -101,11 +134,11 @@ defmodule PickEmWeb.PickEmLive.Index do
   end
 
   def get_team_button_class("selected") do
-    "bg-nd-pink text-nd-yellow border-0 shadow-lg md:ml-2 hover:bg-nd-pink focus:bg-nd-pink"
+    "bg-nd-pink text-nd-yellow leading-none border-2 border-white hover:border-white focus:border-white md:mx-2 hover:bg-nd-pink focus:bg-nd-pink"
   end
 
   def get_team_button_class(_) do
-    "bg-white text-nd-purple border-0 shadow-lg md:ml-2 hover:bg-nd-pink focus:bg-nd-pink"
+    "bg-white text-nd-purple leading-none border-0 md:mx-2 hover:bg-nd-pink focus:bg-nd-pink"
   end
 
   defp get_selected_team(nil), do: {nil, false}
