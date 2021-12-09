a7b704854d95d1b6f32a7128afe1e9f6a9ffdb50
kinson <swilkinsonhunter@gmail.com>
2021-12-09 09:40:52 -0700

    Update deploy script to migrate before restart

--- deploy.sh
+++ deploy.sh
@@ -5,12 +5,16 @@ mix deps.get --only prod
 
 mix assets.deploy
 mix release --overwrite
-mix ecto.migrate
 
 echo "Stopping application"
 _build/prod/rel/classic_clips/bin/classic_clips stop
 
-sleep 3
+sleep 1
+
+echo "Migrating"
+mix ecto.migrate
+
+sleep 1
 
 echo "Starting application"
 _build/prod/rel/classic_clips/bin/classic_clips daemon
