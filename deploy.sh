cf4f87fed9a07481bced6ecf6bd9bcdacfab6a70
kinson <swilkinsonhunter@gmail.com>
2021-12-12 21:45:23 -0700

    Update deploy script (again)

--- deploy.sh
+++ deploy.sh
@@ -1,20 +1,18 @@
 source .env
 export MIX_ENV=prod
 
-mix deps.get --only prod
-
-mix assets.deploy
-mix release --overwrite
-
 echo "Stopping application"
 _build/prod/rel/classic_clips/bin/classic_clips stop
 
-sleep 1
+echo "Fetching latest deps"
+mix deps.get --only prod
+
+echo "Building application"
+mix assets.deploy
+mix release --overwrite
 
 echo "Migrating"
 mix ecto.migrate
-
-sleep 1
 
 echo "Starting application"
 _build/prod/rel/classic_clips/bin/classic_clips daemon
