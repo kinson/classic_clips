b01a561062d857d7e670671b497310245922af35
kinson <swilkinsonhunter@gmail.com>
2021-12-21 19:14:43 -0700

    Let people with short emails sign up

--- lib/classic_clips/timeline/user.ex
+++ lib/classic_clips/timeline/user.ex
@@ -7,7 +7,7 @@ defmodule ClassicClips.Timeline.User do
 
   @primary_key {:id, :binary_id, autogenerate: true}
   schema "users" do
-    field :active, :boolean, default: false
+    field :active, :boolean, default: true
     field :email, :string
     field :username, :string
     field :google_id, :string
@@ -20,8 +20,25 @@ defmodule ClassicClips.Timeline.User do
     [username, _rest] = String.split(attrs.email, "@")
     email_as_initial_user_name = Map.put(attrs, :username, username)
 
-    changeset(%User{}, email_as_initial_user_name)
+    %User{}
+    |> create_user_changeset(email_as_initial_user_name)
     |> Repo.insert(returning: true)
+  end
+
+  def create_user_changeset(user, attrs) do
+    user
+    |> cast(attrs, [:username, :email, :active, :google_id])
+    |> put_change(:google_id, user.google_id || attrs.sub)
+    |> validate_required([:email])
+    |> unsafe_validate_unique(:username, Repo, message: "Username is already taken")
+    |> validate_format(:username, ~r/[0-9a-zA-Z-_]+$/,
+      message: "Username can only contain letters, numbers, underscores, and dashes"
+    )
+    |> unique_constraint(:email, message: "This email is being used by another account")
+    |> unique_constraint(:google_id,
+      message: "This Google Account is already associated with another account"
+    )
+    |> unsafe_validate_unique(:username, Repo, message: "Username is already taken")
   end
 
   @doc false
