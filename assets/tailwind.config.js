990c5ca72c013f0416bdc1345b3e77fedc1ce635
kinson <swilkinsonhunter@gmail.com>
2022-01-15 19:09:56 -0700

    Starting...

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -7,7 +7,10 @@ module.exports = {
           'beef-red': 'rgb(215, 25, 33)',
           'nd-yellow': '#DAFF4F',
           'nd-pink': '#E934D7',
-          'nd-purple': '#6000AA'
+          'nd-purple': '#6000AA',
+
+          'beef-white': '#f6f8fa',
+
         },
         fontFamily: {
           'open-sans': ['Open Sans', 'sans-serif']
@@ -19,6 +22,14 @@ module.exports = {
         },
         boxShadow: {
           'brutal': '4px 5px 0px #111'
+        },
+      gridTemplateColumns: {
+        'bb-sm': '5rem 1fr',
+         'bb-md': '15% 1fr'
+        },
+      gridTemplateRows: {
+        'bb-sm': '3rem 15rem 1fr',
+        'bb-md': '5rem 1fr',
         }
     }
   },
