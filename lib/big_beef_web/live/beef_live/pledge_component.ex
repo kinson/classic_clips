5a404e346c9ba04da4ce772c48276f2a5856dc79
kinson <1526888+kinson@users.noreply.github.com>
2022-12-28 05:58:51 -0800

    Update big beef pledge amount for Lukas first beef (and Sabonis beef)

--- lib/big_beef_web/live/beef_live/pledge_component.ex
+++ lib/big_beef_web/live/beef_live/pledge_component.ex
@@ -12,10 +12,10 @@ defmodule BigBeefWeb.BeefLive.PledgeComponent do
       <div class="pledge-card">
         <div class="raised">
           <p class="amount">
-            $353
+            $395
           </p>
           <p class="description">
-            donated as of December 21st, 2022 ❄️
+            donated as of December 28th, 2022 ❄️
           </p>
         </div>
 
